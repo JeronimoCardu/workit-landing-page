e22b9bcd533af5895f88225a681869ea1cb580f9
Jeronimo Cardu <jeronimocardu29@gmail.com>
2025-04-04 15:49:40 -0300

    feat: characteristics complete

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,33 @@ import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 
 export default function App() {
+  const characteristicsList = [
+    {
+      number: 1,
+      title: "Actionables insights",
+      description:
+        "Optimize your products, improve customers satisfaction and stay ahead of the competition with our product data analytics.",
+    },
+    {
+      number: 2,
+      title: "Data-driven decisions",
+      description:
+        "Make data-driven decisions with our product data analytics. Our AI-generated reports help your unlock insights hidden in your product data.",
+    },
+    {
+      number: 3,
+      title: "Always affordable",
+      description:
+        "Always affordable pricing that scales with your business. Get top-quality product data analytics services without hidden costs or unexepected fees.",
+    },
+  ];
   return (
     <>
       <header>
         <Hero />
       </header>
-      <main>
-        <Characteristics />
+      <main className="bg-purple100 py-10">
+        <Characteristics list={characteristicsList} />
       </main>
       <section>
         <Ad />
--- src/components/Characteristics.tsx
+++ src/components/Characteristics.tsx
@@ -1,3 +1,28 @@
-export default function Characteristics() {
-  return <main className="bg-purple100"></main>;
+type Props = {
+  list: {
+    number: number;
+    title: string;
+    description: string;
+  }[];
+};
+
+export default function Characteristics({ list }: Props) {
+  return (
+    <section className="mx-auto flex justify-evenly">
+      {list.map((charact, index) => (
+        <article
+          className="flex w-[22em] flex-col items-center text-center"
+          key={index}
+        >
+          <p className="text-purple500 mb-[3.5rem] flex h-[3.5rem] w-[3.5rem] items-center justify-center rounded-full border-1 p-6 text-center font-[Fraunces144] text-[1.5rem] leading-10">
+            {charact.number}
+          </p>
+          <p className="textPreset3 text-purple900 mb-[1.75rem]">
+            {charact.title}
+          </p>
+          <p className="textPreset4 text-purple900">{charact.description}</p>
+        </article>
+      ))}
+    </section>
+  );
 }
